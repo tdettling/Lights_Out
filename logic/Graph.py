a63b03bf0d4f78283643406568d21553d6d58e29
L Dettling <tdettling@tds.net>
2022-11-14 12:06:53 -0500

    updates to Graph.py

--- logic/Graph.py
+++ logic/Graph.py
@@ -11,17 +11,91 @@ that are either integers modulo some integer k or integers representing group
 elements as described below.  It also includes a boolean variable "win" that
 tells us if the "Lights Out" game has been won."""
 
+error_dict = {
+    1 : "Invalid Input",
+    2 : "Connection(s) does not exist",
+    3 : "Vertex does not exist",
+    4 : "Cannot create vertex - it already exists"
+}
 class Graph(object):
     """Creates a graph, describing the number of vertices (num_vert) as well as
         the edges (adj_mat or edge_set)"""
-    def __init__(self, adj_dict):
+    def __init__(self,max_vertex_value):
         # number of vertices, adjacency matrix, edge set
-        self.adj_dict = adj_dict
+        self.edge_dict = {}
+        self.max_vertex_value = max_vertex_value
+        self.vertex_values = {}
+        self.throw_error = "No Error Present"
         
-    def printAdjacency(self):
-        for vert in self.adj_dict:
-            print (vert)
+    def printError(self, error_code):
+        global error_dict
+
+        temp_contains = False
+        for key in  error_dict:
+            if key == error_code:
+                temp_contains = True
+                print(str(error_dict[error_code]))
+        if not temp_contains:
+            print("Error code does not exist")
     
+    def printAdjacency(self):
+        for vert in self.edge_dict:
+            print (vert)
+
+
+    def getConnections(self, vertex_name):
+        if vertex_name not in self.edge_dict:
+            self.printError(3)
+            return -1
+        else:
+            edgeSet = ""
+            temp_edge = ""
+            for key in self.edge_dict:
+                if key == vertex_name:
+                    temp_edge = self.edge_dict[key]
+            if len(temp_edge) == 0:
+                self.printError(2)
+                return -1
+    
+            return edgeSet
+
+    def parseEdges(self, vertex_name):
+        if vertex_name in self.edge_dict:
+            edges = self.edge_dict[vertex_name]
+        else:
+            self.printError(3)
+            return
+
+        if edges == "null":
+            self.printError(2)
+            return
+        else: 
+            edgeList = edges.split(',')
+            return edgeList
+
+
+    def addVertex(self, vertex_name, connection = "null", vertex_value = 1):
+        if connection not in self.edge_dict and connection != "null":
+            print("fixme")
+            return
+        for key in self.vertex_values:
+            if key == vertex_name:
+                self.printError(4)
+                return
+            
+        self.vertex_values[vertex_name] = vertex_value
+        self.edge_dict[vertex_name] = connection
+        
+        
+    def removeVertex(self, vertex_name):
+        del self.adj_dict[vertex_name]
+
+    def editVertexValue(vertex_name):
+        pass
+
+    def editVertexConnection(vertex_name, new_start, new_finish):
+        pass
+
     def checkAdjacency(self):
         # checks consistency of adjacency matrix
         result = True
@@ -39,3 +113,33 @@ class Graph(object):
                     result = False
                     print ("You have a loop on vertex" + str(x) + ".")
         return result
+
+    def changeConnection(self):
+        pass
+
+    def toggleVertex(self):
+        pass
+
+    def checkWinner(self):
+        for key in self.vertex_values:
+            if self.vertex_values[key] != 0:
+                return False
+        return True
+
+
+def main_run():
+    graph = Graph(1)
+    print("*************************")
+    graph.addVertex("A", "B", 1)
+    print("*************************")
+    graph.addVertex("A")
+    print("create A")
+    graph.addVertex("B", "A", 0)
+    print("create b")
+    graph.addVertex("C", "A,B")
+    print("create c")
+    print("*************************")
+    graph.addVertex("A", "C", 1)
+    print("*************************")
+
+main_run()
